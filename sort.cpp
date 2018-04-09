7dfccdc9771ea976abdcbdd45b3dae9cd3406e72
jingchunzhen <jingchunzhen@126.com>
2018-04-09 23:59:16 +0800

    add sth

--- sort.cpp
+++ sort.cpp
@@ -9,14 +9,57 @@ using namespace std;
 class Sort
 {
 public:    
+    void bubble_sort(vector<int>&a);
     vector<int> merge(vector<int>left, vector<int>right);
     vector<int> merge_sort(vector<int>a, int start, int end);
-
+    void quick_sort(vector<int>&a, int start, int end);
+    int partition(vector<int>a, int start, int end);
     vector<int> heap_sort(vector<int>a);
-    void heap_adjust();
+    void heaplify();
 private:
 
 };
+
+// quick sort 
+void Sort::quick_sort(vector<int>a, int start, int end)
+{
+    if (start < end)
+    {
+        int mid = Sort::partition(a, start, end);
+        Sort::quick_sort(a, start, mid);
+        Sort::quick_sort(a, mid + 1, end);            
+    }
+}
+
+int Sort::partition(vector<int>a, int start, int end)
+{
+    int pivot = a[start];
+    while (start < end)
+    {
+        while (a[end] >= pivot) end --;
+        while (a[start] <= pivot) start ++;
+        swap(a[start], a[end]);
+    }
+    return start;
+}
+
+// bubble sort
+void Sort::bubble_sort(vector<int>&a)
+{    
+    for (int i = 0; i < a.size(); i ++)
+    {
+        bool flag = true;
+        for (int j = a.size() - 1; j > i; j --)
+        {
+            if (a[j] < a[j - 1])
+            {
+                flag = false;
+                std::swap(a[j], a[j - 1]);
+            }
+        }
+        if (flag) break;
+    }
+}
 
 // merge sort 
 vector<int> Sort::merge_sort(vector<int>a, int start, int end)
@@ -56,7 +99,20 @@ vector<int> Sort::merge(vector<int>left, vector<int>right)
     return res;
 }
 
-int main()
+// heap sort
+vector<int> Sort::heap_sort(vector<int>a)
+{
+    vector<int> res;
+    return res;
+
+}
+
+void Sort::heaplify()
+{
+
+}
+
+int test_merge_sort()
 {
     int ta[] = {3,4,6,7,8,6,2,5,7,8,6,7,8,2,3,5};
     vector<int> a (ta, ta + 16); // 2,7,8,3,4,6,7,8,6,2,5,7,8,6,7,8,2,3,5};   
@@ -71,4 +127,15 @@ int main()
     for (int i = 0; i < a.size(); i ++) cout << a[i] << " ";
     cout << endl;
     return 0;
+}
+
+int main()
+{
+    int ta[] = {3,4,6,7,8,6,2,5,7,8,6,7,8,2,3,5};
+    vector<int> a (ta, ta + 16); // 2,7,8,3,4,6,7,8,6,2,5,7,8,6,7,8,2,3,5};
+    Sort* sort = new Sort();
+    sort->bubble_sort(a);
+    for (int i = 0; i < a.size(); i ++) cout << a[i] << " ";
+    cout << endl;
+    return 0;   
 }
